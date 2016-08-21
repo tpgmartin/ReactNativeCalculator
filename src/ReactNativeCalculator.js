309634eee0321f62cad11ff8bf843d7fd8cbf47f
Tom Martin <tom@tpgmartin.com>
2016-08-21 18:04:09 +0100

    Add button layout, styles

--- src/ReactNativeCalculator.js
+++ src/ReactNativeCalculator.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
 import {
+    AppRegistry,
     Text,
-    AppRegistry
+    View
 } from 'react-native'
+import InputButton from './InputButton'
+import Style from './Style'
+
+const inputButtons = [
+  [1, 2, 3, '/'],
+  [4, 5, 6, '*'],
+  [7, 8, 9, '-'],
+  [0, '.', '=', '+']
+]
 
 class ReactNativeCalculator extends Component {
 
     render() {
-        return (
-            <Text>Hello, React!</Text>
-        )
+      return (
+        <View style={Style.rootContainer}>
+          <View style={Style.displayContainer}></View>
+          <View style={Style.inputContainer}>
+            {this._renderInputButtons()}
+          </View>
+        </View>
+      )
+    }
+
+    _renderInputButtons () {
+      let views = []
+
+      for (let r=0; r<inputButtons.length; r++) {
+        let row = inputButtons[r]
+        let inputRow = []
+        for (let i=0; i<row.length; i++) {
+          let input = row[i]
+
+          inputRow.push(
+            <InputButton value={input} />
+          )
+        }
+
+        views.push(<View style={Style.inputRow}>{inputRow}</View>)
+      }
+
+      return views
     }
 
 }
